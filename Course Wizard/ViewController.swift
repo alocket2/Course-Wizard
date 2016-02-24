788cb885d61c1846aed38635098a3ff6c14e453e
Anthony Lockett <alocket2@fau.edu>
2016-02-23 21:07:24 -0500

    Initial Commit

---
+++ Course Wizard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Course Wizard
+//
+//  Created by Anthony Lockett on 2/23/16.
+//  Copyright © 2016 Anthony Lockett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
