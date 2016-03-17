e7d1a3dde504d9126a9dd9a145066ff97fe542e9
Anthony Lockett <alocket2@fau.edu>
2016-03-16 23:34:53 -0400

    Updated tab bar UI

---
+++ Course Wizard/UIImage+.swift
@@ -0,0 +1,30 @@
+//
+//  UIImage+.swift
+//  Course Wizard
+//
+//  Created by Anthony Lockett on 3/16/16.
+//  Copyright © 2016 Anthony Lockett. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    func imageWithColor(tintColor: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        
+        let context = UIGraphicsGetCurrentContext()! as CGContextRef
+        CGContextTranslateCTM(context, 0, self.size.height)
+        CGContextScaleCTM(context, 1.0, -1.0);
+        CGContextSetBlendMode(context, CGBlendMode.Normal)
+        
+        let rect = CGRectMake(0, 0, self.size.width, self.size.height) as CGRect
+        CGContextClipToMask(context, rect, self.CGImage)
+        tintColor.setFill()
+        CGContextFillRect(context, rect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
