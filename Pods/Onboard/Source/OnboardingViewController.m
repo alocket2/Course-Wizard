f952550e4ded063baf6adc92d42d70dc2539ef78
Bumangues <daniel@daniels-macbook-pro-2.local>
2016-03-29 18:39:30 -0400

    pods

--- Pods/Onboard/Source/OnboardingViewController.m
+++ Pods/Onboard/Source/OnboardingViewController.m
@@ -27,6 +27,9 @@ static NSString * const kSkipButtonText = @"Skip";
     OnboardingContentViewController *_upcomingPage;
 }
 
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self forKeyPath:UIApplicationWillEnterForegroundNotification];
+}
 
 #pragma mark - Initializing with images
 
@@ -90,6 +93,7 @@ static NSString * const kSkipButtonText = @"Skip";
     self.skipButton = [UIButton new];
     [self.skipButton setTitle:kSkipButtonText forState:UIControlStateNormal];
     [self.skipButton addTarget:self action:@selector(handleSkipButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    self.skipButton.titleLabel.adjustsFontSizeToFitWidth = YES;
 
     // create the movie player controller
     self.moviePlayerController = [MPMoviePlayerController new];
